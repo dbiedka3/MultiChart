58b9ed0f77271093b7795eb38672b01e76f1eeeb
danbie <danbie@st.amu.edu.pl>
2022-08-04 16:44:31 +0200

    Constants changed to min required values, closes #11

--- src/main/java/config/GlobalConstants.java
+++ src/main/java/config/GlobalConstants.java
@@ -8,26 +8,26 @@ public interface GlobalConstants {
      * setting horizontal pixel distance
      * between consecutive data points.
      */
-    public static final double CHART_CELL_WIDTH = 5d;
+    public static final double MIN_CHART_CELL_WIDTH = 5d;
 
     /**
      * Defines standard candlestick width in pixels
      */
-    public static final double CANDLESTICK_WIDTH = 3d;
+    public static final double MIN_CANDLESTICK_WIDTH = 3d;
 
     /**
      * Defines chart area padding in pixels
      */
-    public static final double LEFT_PADDING=20d;
-    public static final double RIGHT_PADDING=20d;
-    public static final double TOP_PADDING=20d;
-    public static final double BOTTOM_PADDING=20d;
+    public static final double MIN_LEFT_PADDING=20d;
+    public static final double MIN_RIGHT_PADDING=20d;
+    public static final double MIN_TOP_PADDING=20d;
+    public static final double MIN_BOTTOM_PADDING=20d;
 
     /**
      * Defines chart window minimum size in pixels
      */
     public static final double MIN_CHART_WIDTH=600d;
-    public static final double MIN_CHART_HEIGTH=300d;
+    public static final double MIN_CHART_HEIGHT=300d;
 
 
 
